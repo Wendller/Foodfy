eae0ea1b5340801463f2a8baaa0869c8bdc28acf
Wendler <wendler_mendes@hotmail.com>
2020-03-20 19:52:13 -0300

    Lógica front do gerenciador de imagens

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,72 @@
+//? LOGICA PARA UPLOAD DE IMAGENS
+
+const PhotosUpload = {
+
+  preview: document.querySelector('#photos-preview'),
+  uploadLimit: 5,
+  handleFileInput(event) {
+    const { files: fileList } = event.target;
+    
+    if(PhotosUpload.hasLimit(event)) return
+
+    Array.from(fileList).forEach(file => {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const image = new Image();
+        image.src = String(reader.result);
+
+        const div = PhotosUpload.getContainer(image);
+
+        PhotosUpload.preview.appendChild(div);
+      }
+
+      reader.readAsDataURL(file)
+    });
+  },
+  hasLimit(event) {
+    const { uploadLimit } = PhotosUpload;
+    const { files: fileList } = event.target;
+
+    if(fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+      event.preventDefault();
+      return true
+    }
+
+    return false
+  },
+  getContainer(image) {
+    const div = document.createElement('div');
+        div.classList.add('photo');
+
+        div.onclick = PhotosUpload.removePhoto;
+
+        div.appendChild(image);
+
+        div.appendChild(PhotosUpload.getRemoveButton());
+
+        return div
+  },
+  getRemoveButton() {
+    const button = document.createElement('i');
+    button.classList.add('material-icons');
+    button.innerHTML = "close";
+
+    return button
+  },
+  removePhoto(event) {
+    const photoDiv = event.target.parentNode;
+    const photosArray = Array.from(PhotosUpload.preview.children);
+    const index = photosArray.indexOf(photoDiv);
+
+    photoDiv.remove();
+  }
+  
+}
+
+
+//? LOGICA PARA PAGINACAO
 
 function paginate(selectedPage, totalPages) {
   let pages = [],
@@ -47,7 +116,7 @@ console.log(pages)
 pagination.innerHTML = elements
 
 
-
+//? LOGICA PARA ADICAO DE INGREDIENTES
 
 function addIngredient(classePai, classeFilho, name) {
   const ingredients = document.querySelector(classePai);
